ccdde965c2c8a6419ff68eb5a0319b5260c08e3b
shivalasvegas <shiva.king@academytrainee.com>
2020-10-06 16:43:45 +0100

    SK - Factorial: factorial definition rewritten as tail recursion

--- factorial/src/main/scala/week1/Factorial.scala
+++ factorial/src/main/scala/week1/Factorial.scala
@@ -1,17 +1,21 @@
 package week1
+
+import scala.annotation.tailrec
+
 // design the factorial function so that it uses tail recursion
 //def factorial(n: Int): Int = if (n==0) 1 else n*factorial(n-1)
 object Factorial extends App{
 
-  def factorialPrima(n: Int): Int = if (n==0) 1 else n*factorial(n-1)
+  def factorialUna(n: Int): Int = if (n==0) 1 else n*factorialUna(n-1)
 
-  def recur(first: Int, second: Int) = first*second
-  def factorial(n: Int): Int = {
-
-    if (n==0) 1 else recur(n, factorial(n-1))
+  def factorialDos(n: Int): Int = {
+    @tailrec
+    def loop(acc: Int, n: Int): Int = if (n==0) acc else loop(acc*n, n-1)
+    loop(1, n)
   }
+  
   val x = 4
-  println(s"factorialPrima: the factorial of $x is ${factorialPrima(x)}")
-  println(s"factorial: the factorial of $x is ${factorial(x)}")
+  println(s"factorialPrima: the factorial of $x is ${factorialUna(x)}")
+  println(s"factorial: the factorial of $x is ${factorialDos(x)}")
 
 }
