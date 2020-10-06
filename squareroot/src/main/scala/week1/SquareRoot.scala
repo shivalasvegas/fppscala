be322087eff3f339d0deff80f7496aee0a3d0e38
shivalasvegas <shiva.king@academytrainee.com>
2020-10-06 15:31:13 +0100

    SK - removed x parameters from smaller functions

--- squareroot/src/main/scala/week1/SquareRoot.scala
+++ squareroot/src/main/scala/week1/SquareRoot.scala
@@ -1,25 +1,26 @@
 package week1
 
 object SquareRoot extends App{
+  
   def abs(x: Double) = if (x<0) -x else x
   def square(y: Double): Double = y*y
 
   def sqrt(x: Double): Double = {
 
-    def sqrtIter(guess: Double, x: Double): Double = {
+    def sqrtIter(guess: Double): Double = {
     // if the answer is within error then return guess
     // Otherwise improve the guess
-      if (isGoodEnough (guess, x) ) guess
-      else (sqrtIter (improve (guess, x), x) )
+      if (isGoodEnough(guess) ) guess
+      else (sqrtIter (improve (guess)) )
     }
     // calculates an within an accepted error
-    def isGoodEnough(guess: Double, x: Double) = {
+    def isGoodEnough(guess: Double) = {
       abs (square (guess) - x) / x < 0.0001
     }
     // Newton's method
-    def improve(guess: Double, x: Double): Double = {(guess + x / guess) / 2}
+    def improve(guess: Double): Double = {(guess + x / guess) / 2}
 
-    sqrtIter(1.0, x)
+    sqrtIter(1.0)
     //to avoid name-space pollution the small functions: improve/isGoodEnough/sqrtIter are put inside the function sqrt
     // program is cleaner
     // this uses a block
