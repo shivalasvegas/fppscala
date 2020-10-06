d1ed3772fc28ef22e12b3d2b16336b3fbedb1bee
shivalasvegas <shiva.king@academytrainee.com>
2020-10-06 15:26:36 +0100

    SK - SquareRoot: moved smaller definitions into sqrt function to make code cleaner

--- squareroot/src/main/scala/week1/SquareRoot.scala
+++ squareroot/src/main/scala/week1/SquareRoot.scala
@@ -4,22 +4,28 @@ object SquareRoot extends App{
   def abs(x: Double) = if (x<0) -x else x
   def square(y: Double): Double = y*y
 
-  def sqrtIter(guess: Double, x: Double): Double = {
+  def sqrt(x: Double): Double = {
+
+    def sqrtIter(guess: Double, x: Double): Double = {
     // if the answer is within error then return guess
     // Otherwise improve the guess
-    if (isGoodEnough(guess, x)) guess
-    else (sqrtIter(improve(guess, x), x))
+      if (isGoodEnough (guess, x) ) guess
+      else (sqrtIter (improve (guess, x), x) )
+    }
+    // calculates an within an accepted error
+    def isGoodEnough(guess: Double, x: Double) = {
+      abs (square (guess) - x) / x < 0.0001
+    }
+    // Newton's method
+    def improve(guess: Double, x: Double): Double = {(guess + x / guess) / 2}
+
+    sqrtIter(1.0, x)
+    //to avoid name-space pollution the small functions: improve/isGoodEnough/sqrtIter are put inside the function sqrt
+    // program is cleaner
+    // this uses a block
+    // def sqrt(x: Double) = sqrtIter(1.0, x)
   }
-  // calculates an within an accepted error
-  def isGoodEnough(guess: Double, x: Double) = {
-    abs(square(guess) - x)/x < 0.0001
-  }
-  // Newton's method
-  def improve(guess: Double, x: Double) =
-    (guess + x / guess)/2
-//to avoid name-space pollution the small functions: improve/isGoodEnough/sqrtIter are put inside the function sqrt
- // def sqrt(x: Double) = sqrtIter(1.0, x)
-  val sqrtTest = 0.00001
+  val sqrtTest = 5
   println(s"The square root of $sqrtTest = ${sqrt(sqrtTest)}")
 
 }
